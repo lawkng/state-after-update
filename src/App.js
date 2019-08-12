afd31cb419308a35c27232591ff7e5b1eec00bbd
lawkng <kohen_eleeo@hotmail.com>
2019-08-12 23:06:09 +1000

    Add IE11 dangerouslySetInnerHTML test

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ import { SelectViewUseEffect } from './components/SelectViewUseEffect';
 import SelectViewClassComp from './components/SelectViewClassComp';
 import SelectViewReduxChange from './components/SelectViewReduxChange';
 
+const test = () => {
+  const local = 'This is a test. <strong>{token}</strong>';
+  return local.replace('{token}', 'hello');
+};
+
 function App() {
   return (
     <div className="App">
@@ -14,6 +19,7 @@ function App() {
       <SelectViewUseEffect></SelectViewUseEffect>
       <SelectViewClassComp></SelectViewClassComp>
       <SelectViewReduxChange></SelectViewReduxChange>
+      <div dangerouslySetInnerHTML={{__html: test()}} ></div>
       <Footer></Footer>
     </div>
   );
